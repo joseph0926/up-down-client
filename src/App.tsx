02da6f0e0bc25e660e70138a712be9f0ddec2867
김영훈 <joseph0926@naver.com>
2025-05-26 08:07:59 +0900

    feat.analytics: vercel analytics 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClientProvider } from '@tanstack/react-query';
+import { Analytics } from '@vercel/analytics/react';
 import { RouterProvider } from 'react-router';
 import { Toaster } from 'sonner';
 import { queryClient } from './lib/query-client';
@@ -9,6 +10,7 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <Toaster richColors closeButton position="bottom-right" />
+      <Analytics />
     </QueryClientProvider>
   );
 }
